fd647c9c13a807bcdab45990cca4bdebc2229f4f
Anil Kulkarni <anil@terminal.space>
2023-08-26 20:13:49 -0700

    Correctly handle visiting the website with a fully qualified username

--- app/Services/AccountService.php
+++ app/Services/AccountService.php
@@ -226,6 +226,12 @@ class AccountService
             if (Str::contains($username, '@') && ! Str::startsWith($username, '@')) {
                 $username = "@{$username}";
             }
+            if(preg_match('/^@([^@]+)@'.preg_quote(config('pixelfed.domain.app')).'$/i', $username, $matches)) {
+                # The username is the fully qualified @user@example.com and the pixelfed site is example.com
+                # Normalize this username to just user
+                $username = $matches[1];
+            }
+
             $profile = DB::table('profiles')
                 ->whereUsername($username)
                 ->first();
